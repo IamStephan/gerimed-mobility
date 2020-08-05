6dd098359fa76e6c0588b2a0bfcf820e14157809
Stephan Burger <stephanburger54@gmail.com>
2020-08-05 20:49:24 +0200

    removed bloated package

--- src/components/provider/index.js
+++ src/components/provider/index.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core'
 
-// Notifications
-import { SnackbarProvider } from 'notistack'
-
 // Theme
 import theme from './theme'
 
 const Provider = props => {
   return (
     <ThemeProvider theme={theme}>
-      <SnackbarProvider
-        maxSnack={3}
-      >
-        { props.children }
-      </SnackbarProvider>
+      { props.children }
     </ThemeProvider>
   )
 }
