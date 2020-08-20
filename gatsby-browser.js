17a04010a768f5a0ee949f0a2f2af337f8c390b6
Stephan Burger <stephanburger54@gmail.com>
2020-08-20 23:37:13 +0200

    test something

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 // Components
 import Provider from './src/components/provider'
 
--- gatsby-node.js
+++ gatsby-node.js
@@ -6,12 +6,7 @@
  */
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
-
-  if (page.path.match(/^\/product/)) {
-    page.matchPath = "/product/*"
-    createPage(page)
-  }
-
+  
   if (page.path.match(/^\/profile/)) {
     page.matchPath = "/profile/*"
     createPage(page)
