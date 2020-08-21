6cc35235d0c0c280ec0c7800cc9add7f0819ee4d
Stephan Burger <stephanburger54@gmail.com>
2020-08-21 16:31:01 +0200

    note to self

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,20 @@ import Provider from './src/components/provider'
 import 'sanitize.css'
 import './src/utils/reset.scss'
 
+/**
+ * NOTE TO SELF:
+ * =============
+ * This runs in the browser on initial load of the page
+ * And can lead to some unexpected bugs if the component
+ * state is required immediately
+ */
+
+/**
+* TODO
+* =====
+* - Remove duplicates that are not needed
+*/
+
 function wrapRootElement ({ element, props }) {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,6 +7,19 @@ import Provider from './src/components/provider'
 import 'sanitize.css'
 import './src/utils/reset.scss'
 
+/**
+ * NOTE NOTE TO SELF:
+ * ==================
+ * This is not actually server side rendering
+ * It essentially means that this is compiled at build time
+ */
+
+ /**
+  * TODO
+  * =====
+  * - Remove duplicates that are not needed
+  */
+
 function wrapRootElement ({ element, props }) {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
