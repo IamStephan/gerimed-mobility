a2de879f2bdd2268979653fb3c75bbcda743a01f
Stephan Burger <stephanburger54@gmail.com>
2020-09-26 22:33:49 +0200

    shop filter molecule MVP

---
+++ src/molecules/shop_filter/controller.js
@@ -0,0 +1,209 @@
+import { Machine, assign, actions, send } from 'xstate'
+
+// Gatsby
+import { navigate } from 'gatsby'
+
+// URL
+import { stringify, parse } from 'qs'
+import { isEqual, set } from 'lodash'
+
+const { pure } = actions
+
+function filterFormBuilder(params) {
+  const {
+    categories = {},
+    availability = {},
+    search,
+    min_price,
+    max_price
+  } = params
+
+  // Default Values
+  let query = {}
+  let categoriesArray = []
+  let availabilityArray = []
+
+  // Array-like query
+  for(const key in categories) {
+    if(categories[key]) {
+      categoriesArray.push(key)
+    }
+  }
+
+  for(const key in availability) {
+    if(availability[key]) {
+      availabilityArray.push(key)
+    }
+  }
+
+  // Query Builder
+  if(categoriesArray.length) query.categories = categoriesArray
+  if(availabilityArray.length) query.availability = availabilityArray
+  if(search) query.search = search
+  if(min_price) query.min_price = min_price
+  if(max_price) query.max_price = max_price
+
+  return query
+}
+
+function filterURLBuilder(params) {
+  const {
+    categories = [],
+    availability = [],
+    search,
+    min_price,
+    max_price
+  } = params
+
+  // Default Values
+  let query = {}
+
+  // Query Builder
+  query.categories = categories
+  query.availability = availability
+  if(search) query.search = search
+  if(min_price) query.min_price = min_price
+  if(max_price) query.max_price = max_price
+
+  return query
+}
+
+const LocalController = Machine({
+  id: 'ShopFilterController',
+  context: {
+    setValue: null,
+    getValues: null,
+    categories: [],
+    filters: {}
+  },
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        APPLY_FILTERS: {
+          actions: ['applyFilters', 'navigateWithFilters']
+        },
+
+        /**
+         * NOTE:
+         * =====
+         * These events should not touch the navbar
+         */
+        CHECK_FILTERS: {
+          actions: 'checkFilters'
+        },
+        MATCH_FILTERS: {
+          actions: ['matchFiltersWithForm']
+        }
+      }
+    }
+  }
+}, {
+  actions: {
+    applyFilters: assign({
+      filters: (_context, event) => {
+        // When filters are applied reset the page aswell since it can change the results
+        const { data } = event
+  
+        return filterFormBuilder(data)
+      }
+    }),
+    navigateWithFilters: (context) => {
+      let filterString = stringify(context.filters)
+      
+      navigate(`?${filterString}`)
+    },
+    checkFilters: pure((context) => {
+      // Gatsby SSR
+      if(typeof window === 'undefined') return send('IDLE')
+  
+      const search = window.location.search.substring(1)
+      let filters
+  
+      try {
+        filters = parse(search)
+      } catch(e) {
+        filters = context.filters
+      }
+
+      const filtersBuilt = filterURLBuilder(filters)
+  
+      if(!isEqual(context.filters, filtersBuilt)) {
+        return send('MATCH_FILTERS', {
+          data: filtersBuilt
+        })
+      }
+    }),
+    matchFiltersWithForm: assign({
+      filters: (context, event) => {
+        const { setValue, getValues, categories, filters } = context
+        // Gatsby SSR
+        if(typeof window === 'undefined') return
+
+        // Current From Data
+        const formData = getValues()
+
+        // Get url params
+        const search = window.location.search.substring(1)
+
+        let preFilters
+        try {
+          preFilters = parse(search)
+        } catch(e) {
+          preFilters = context.filters
+        }
+
+        const filtersBuilt = filterURLBuilder(preFilters)
+
+        // No need to update since its the same
+        if(isEqual(context.filters, filtersBuilt)) return
+
+        // Search
+        if(formData.search !== filtersBuilt.search) {
+          setValue('search', filtersBuilt.search, {
+            isDirty: true
+          })
+        }
+
+        // Price
+        if(formData.min_price !== filtersBuilt.min_price) {
+          setValue('min_price', filtersBuilt.min_price, {
+            isDirty: true
+          })
+        }
+        if(formData.max_price !== filtersBuilt.max_price) {
+          setValue('max_price', filtersBuilt.max_price, {
+            isDirty: true
+          })
+        }
+
+        // Categories
+        categories.forEach((category) => {
+          setValue('categories', {
+            [category.name]: !!filtersBuilt.categories.includes(category.name)
+          }, {
+            isDirty: true
+          })
+        })
+        
+        // Availability
+        setValue('availability', {
+          available: !!filtersBuilt.availability.includes('available')
+        }, {
+          isDirty: true
+        })
+        setValue('availability', {
+          instock: !!filtersBuilt.availability.includes('instock')
+        }, {
+          isDirty: true
+        })
+
+        return filters
+      }
+    })
+  }
+})
+
+export {
+  LocalController
+}
--- src/molecules/shop_filter/index.js
+++ src/molecules/shop_filter/index.js
@@ -1,14 +1,344 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+// Gatsby
+import { useStaticQuery, graphql, Link } from 'gatsby'
+
+// Material
+import {
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+  InputAdornment,
+  Divider,
+  ButtonGroup,
+  Button,
+  Collapse,
+  IconButton,
+} from '@material-ui/core'
+import {
+  SearchOutlined,
+  ExpandMoreOutlined
+} from '@material-ui/icons'
+
+// Hooks
+import { useMachine } from '@xstate/react'
+import { useForm, Controller } from 'react-hook-form'
+import { useLocation } from '@reach/router'
+
+// Controller
+import { LocalController } from './controller'
 
 // Styles
 import styles from './styles.module.scss'
 
-const ShopFilter = () => {
+const STATIC_QUERY = graphql`
+  query {
+    strapiCategories {
+      categories {
+        name
+        id
+      }
+    }
+  }
+`
+
+const SideTrackSection = props => {
+  const {
+    isOpen,
+    setOpen,
+    title,
+    children
+  } = props
+
+  return (
+    <div
+      className={styles['sideTrackSection']}
+    >
+      <div
+        className={styles['sectionHeader']}
+        onClick={setOpen}
+      >
+        <Typography
+          variant='overline'
+          color='secondary'
+        >
+          <b>{title}</b>
+        </Typography>
+
+        <IconButton
+          size='small'
+          color='secondary'
+        >
+          <ExpandMoreOutlined
+            className={`${styles['dropdown']} ${styles[isOpen ? 'open' : 'close']}`}
+          />
+        </IconButton>
+      </div>
+      
+      <div
+        className={styles['sectionContent']}
+      >
+        <Collapse
+          in={isOpen}
+        >
+          {children}
+        </Collapse>
+      </div>
+      
+    </div>
+  )
+}
+
+const Split = ({ divide }) => (
+  <>
+    <br />
+    {divide && (
+        <>
+          <Divider />
+          <br />
+        </>
+      )}
+  </>
+)
+
+const SearchBar = props => {
+  return (
+    <div
+      className={styles['roundedInput']}
+    >
+      <TextField
+        InputProps={{
+          classes: {
+            root: styles['inputBox']
+          },
+          startAdornment: (
+            <InputAdornment
+              position='start'
+            >
+              <SearchOutlined />
+            </InputAdornment>
+          )
+        }}
+        fullWidth
+        variant='outlined'
+        size='small'
+        color='secondary'
+        label='Search Products'
+        {...props}
+      />
+    </div>
+  )
+}
+
+const PriceInput = props => {
+  return (
+    <div
+      className={`${styles['roundedInput']} ${styles['price']}`}
+    >
+      <TextField
+        InputProps={{
+          classes: {
+            root: styles['inputBox']
+          }
+        }}
+        variant='outlined'
+        size='small'
+        type='number'
+        color='secondary'
+        {...props}
+      />
+    </div>
+  )
+}
+
+const ShopFilter = (props) => {
+  const { strapiCategories: { categories } } = useStaticQuery(STATIC_QUERY)
+
+  const location = useLocation()
+
+  const { handleSubmit, register, setValue, getValues, reset, control } = useForm()
+
+  const [current, send] = useMachine(LocalController, {
+    context: {
+      setValue,
+      getValues,
+      categories
+    }
+  })
+
+  useEffect(() => {
+    send('CHECK_FILTERS')
+  }, [location.search])
+
+  // TEMP!!
+  const [showPrice, setShowPrice] = useState(false)
+  const [showCategories, setShowCategories] = useState(false)
+  const [showAvailability, setShowAvailability] = useState(false)
+
+  function _applyFilters(data) {
+    send('APPLY_FILTERS', {
+      data
+    })
+  }
+
   return (
     <div
       className={styles['shopFilterContainer']}
     >
-      asdasd
+      <form
+        onSubmit={handleSubmit(_applyFilters)}
+        className={styles['sideTrackFilter']}
+      >
+        <div>
+          <Typography
+            color='secondary'
+            variant='h4'
+          >
+            <b>Filters</b>
+          </Typography>
+        </div>
+
+        <Split />
+
+        <Controller
+          control={control}
+          defaultValue=''
+          name={'search'}
+          render={({ onChange, onBlur, value, name }) => (
+            <SearchBar
+              onBlur={onBlur}
+              onChange={e => onChange(e.target.value)}
+              value={value}
+              name={name}
+            />
+          )}
+        />
+
+        <Split divide />
+
+        <SideTrackSection
+          title='Price'
+          isOpen={showPrice}
+          setOpen={() => setShowPrice((prev) => !prev)}
+        > 
+          <PriceInput
+            name='min_price'
+            inputRef={register}
+            label='Min Price'
+          />
+          <PriceInput
+            name='max_price'
+            inputRef={register}
+            label='Max Price'
+          />
+        </SideTrackSection>
+
+        <SideTrackSection
+          title='Categories'
+          isOpen={showCategories}
+          setOpen={() => setShowCategories((prev) => !prev)}
+        >
+          <FormGroup>
+              {
+                categories.map((category) => (
+                  <Controller
+                    control={control}
+                    defaultValue={false}
+                    name={'categories.' + category.name}
+                    render={({ onChange, onBlur, value, name }) => (
+                      <FormControlLabel
+                        key={category.id}
+                        control={(
+                          <Checkbox
+                            onBlur={onBlur}
+                            onChange={e => onChange(e.target.checked)}
+                            checked={value}
+                            name={name}
+                          />
+                        )}
+                        label={category.name}
+                      />
+                    )}
+                  />
+
+                ))
+              }
+          </FormGroup>
+        </SideTrackSection>
+
+        <SideTrackSection
+          title='Availability'
+          isOpen={showAvailability}
+          setOpen={() => setShowAvailability((prev) => !prev)}
+        >
+          <FormGroup>
+            <Controller
+              control={control}
+              defaultValue={false}
+              name='availability.available'
+              render={({ onChange, onBlur, value, name }) => (
+                <FormControlLabel
+                  control={(
+                    <Checkbox
+                      onBlur={onBlur}
+                      onChange={e => onChange(e.target.checked)}
+                      checked={value}
+                      name={name}
+                    />
+                  )}
+                  label='Available'
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              defaultValue={false}
+              name='availability.instock'
+              render={({ onChange, onBlur, value, name }) => (
+                <FormControlLabel
+                  control={(
+                    <Checkbox
+                      onBlur={onBlur}
+                      onChange={e => onChange(e.target.checked)}
+                      checked={value}
+                      name={name}
+                    />
+                  )}
+                  label='In Stock'
+                />
+              )}
+            />
+          </FormGroup>
+        </SideTrackSection>
+
+        <div>
+          <ButtonGroup
+            color='secondary'
+            disableElevation
+            fullWidth
+          >
+            <Button
+              variant='contained'
+              type='submit'
+            >
+              Apply
+            </Button>
+
+            <Button
+              component={Link}
+              to='?'
+            >
+              Clear
+            </Button>
+          </ButtonGroup>
+          
+        </div>
+
+        
+      </form>
     </div>
   )
 }
