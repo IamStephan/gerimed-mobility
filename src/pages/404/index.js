9cb0cae890c97e59a7914e1f2021dc0fd69d4f93
Stephan Burger <stephanburger54@gmail.com>
2020-10-28 21:49:29 +0200

    redirect on 404 (Should do this on netlify)

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,37 +1,14 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 
-// Constants
-import { MODE as FOOTERMODE } from '../../constants/footer'
-
-// Templates
-import Layout from '../../templates/content_layout'
-
-// Constants
-import { PAGES } from '../../constants/pages'
-
-// Sections
-import NotFoundSection from '../../sections/notFound'
-
-// Styles
-import styles from './styles.module.scss'
-
-// SEO
-import SEO from '../../molecules/seo'
+// Gatsby
+import { navigate } from 'gatsby'
 
 const NotFound = () => {
-  return (
-    <Layout
-      page={PAGES.notfound}
-      footerMode={FOOTERMODE.normal}
-      className={styles['notFoundPage']}
-    >
-      <SEO
-        title='Page Not Found'
-        description='Gerimed Mobility Home'
-      />
-      <NotFoundSection />
-    </Layout>
-  )
+  useLayoutEffect(() => {
+    navigate('/')
+  }, [])
+
+  return null
 }
 
 export default NotFound
